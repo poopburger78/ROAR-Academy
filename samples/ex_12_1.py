058015d3f306162ce4da27d09758bc6e0bc05344
poopburger78 <poopburger78@gmail.com>
2024-07-29 13:16:36 -0700

    update ex_12_1

--- samples/ex_12_1.py
+++ samples/ex_12_1.py
@@ -1,5 +1,17 @@
 
 import numpy as np
-
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 v = np.array([2., 2., 4.])
+
+fig = plt.figure()
+a = fig.add_subplot(1,1,1, projection = '3d')
+
+a.set_xlabel('e0')
+a.set_ylabel('e1')
+a.set_zlabel('e2')
+
+plt.plot(v[0], v[1], v[2], 'ro')
+
+plt.show()
